6b0b5d7635b9bbadc5487a8e06e06fff4ebb378e
RiyoGod <godsxrio@gmail.com>
2025-05-01 19:16:11 +0530

    Create change.py

---
+++ change.py
@@ -0,0 +1,12 @@
+import requests
+
+BOT_TOKEN = "7841641161:AAGOGBmvkVQECpc5e6xpATEqKmKpF5IW8kU"
+NEW_NAME = "Instagram Hacker"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/setMyName"
+payload = {
+    "name": NEW_NAME
+}
+
+response = requests.post(url, json=payload)
+print(response.json())
