4bdc767bb661bc6f4fde8f5c31367af46df2177d
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-05-06 01:00:21 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -170,7 +170,7 @@ async def command_handler(event):
         )
 
 
-elif cmd == "broadcast":
+ elif cmd == "broadcast":
     if event.reply_to_msg_id:
         reply_msg = await event.get_reply_message()
         count = 0
