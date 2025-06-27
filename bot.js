8ceab721fcc8271573ebd3672f7bdcd81dfa1e62
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-06-27 13:54:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 /**
- * Farm Bot with Vanilla Wood Collection, Crafting Table & Boat Crafting
- * No external collectblock plugin needed
+ * Farm Bot with Direct Go-To Command
+ * Author: RareAura Automation Setup
  */
 
 const mineflayer = require('mineflayer');
@@ -15,7 +15,7 @@ const bot = mineflayer.createBot({
 
 bot.loadPlugin(pathfinder);
 
-bot.on('spawn', async () => {
+bot.on('spawn', () => {
   const mcData = require('minecraft-data')(bot.version);
   const defaultMove = new Movements(bot, mcData);
 
@@ -23,13 +23,7 @@ bot.on('spawn', async () => {
   defaultMove.canSwim = true;
   defaultMove.liquidCost = 1;
 
-  bot.chat('Bot online. Collecting wood...');
-  await collectWood(defaultMove);
-
-  // Move to farm coords
-  const farmGoal = new goals.GoalBlock(-422, 64, -1480);
-  bot.pathfinder.setMovements(defaultMove);
-  bot.pathfinder.setGoal(farmGoal);
+  bot.chat('Bot online. Ready for commands.');
 
   setInterval(() => {
     if (bot.food < 15) eatFood();
@@ -69,8 +63,14 @@ bot.on('chat', async (username, message) => {
 
   if (message === 'tcords') {
     const pos = bot.entity.position;
-    const dist = pos.distanceTo({ x: -422, y: 64, z: -1480 });
-    bot.chat(`I am at X:${pos.x.toFixed(1)} Y:${pos.y.toFixed(1)} Z:${pos.z.toFixed(1)} | ${dist.toFixed(1)} blocks from farm.`);
+    bot.chat(`I am at X:${pos.x.toFixed(1)} Y:${pos.y.toFixed(1)} Z:${pos.z.toFixed(1)}`);
+  }
+
+  if (message === 'goto') {
+    bot.chat('Going to farm coordinates...');
+    const farmGoal = new goals.GoalBlock(-422, 64, -1480);
+    bot.pathfinder.setMovements(defaultMove);
+    bot.pathfinder.setGoal(farmGoal);
   }
 });
 
@@ -95,70 +95,6 @@ function eatFood() {
     });
   } else {
     bot.chat('No food found!');
-  }
-}
-
-// ====== Collect Wood (Vanilla) ======
-async function collectWood(defaultMove) {
-  try {
-    const mcData = require('minecraft-data')(bot.version);
-
-    // Find nearest log
-    const logBlock = bot.findBlock({
-      matching: block => block.name.includes('log'),
-      maxDistance: 32
-    });
-
-    if (!logBlock) {
-      bot.chat('No logs nearby to make a boat.');
-      return;
-    }
-
-    // Move to log
-    bot.pathfinder.setMovements(defaultMove);
-    const goal = new goals.GoalBlock(logBlock.position.x, logBlock.position.y, logBlock.position.z);
-    await bot.pathfinder.goto(goal);
-
-    // Dig log
-    await bot.dig(logBlock);
-    bot.chat('Collected wood.');
-
-    // Craft planks
-    const plankRecipe = bot.recipesFor(mcData.itemsByName.oak_planks.id, null, 1)[0];
-    if (plankRecipe) {
-      await bot.craft(plankRecipe, 1, null);
-      bot.chat('Crafted planks.');
-    }
-
-    // Craft crafting table
-    const craftingTableRecipe = bot.recipesFor(mcData.itemsByName.crafting_table.id, null, 1)[0];
-    if (craftingTableRecipe) {
-      await bot.craft(craftingTableRecipe, 1, null);
-      bot.chat('Crafted crafting table.');
-    }
-
-    // Place crafting table
-    const craftingTableItem = bot.inventory.items().find(i => i.name.includes('crafting_table'));
-    if (craftingTableItem) {
-      const targetPos = bot.entity.position.offset(1, -1, 0);
-      await bot.equip(craftingTableItem, 'hand');
-      await bot.placeBlock(bot.blockAt(targetPos), new Vec3(1, 0, 0));
-      bot.chat('Placed crafting table.');
-    }
-
-    // Craft boat using placed crafting table
-    const boatRecipe = bot.recipesFor(mcData.itemsByName.oak_boat.id, null, 1)[0];
-    if (boatRecipe) {
-      const craftingTableBlock = bot.findBlock({
-        matching: mcData.blocksByName.crafting_table.id,
-        maxDistance: 5
-      });
-      await bot.craft(boatRecipe, 1, craftingTableBlock);
-      bot.chat('Crafted boat.');
-    }
-
-  } catch (err) {
-    bot.chat('Error collecting wood or crafting: ' + err.message);
   }
 }
 
