39da50911386ce22a9c55a76317ca735402ac9b8
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-07-08 23:50:01 +0530

    Update lyric.py

--- lyric.py
+++ lyric.py
@@ -3,7 +3,7 @@ from azapi import AZlyrics
 # Names should be passed corrrectly
 # If you can't remember, use google or duckduckgo
 # AZlyrics('google')
-api = AZlyrics()
+api = AZlyrics('google')
 
 api.artist = "Taylor Swift"
 api.title = "Blank Space"
